279c6fbc072987d3b28fe95dd48f832876458d22
MohamadSolkhanNawawi <mohamadsolkhan558@gmail.com>
2025-10-28 23:22:52 +0700

    feat(js): add examples for JavaScript Callback Functions

---
+++ Frontend-Foundations/JavaScript-Fundamental-From-Beginner-to-Advanced/callbacksFunction.js
@@ -0,0 +1,12 @@
+// SetTimeout is used to execute a function after
+// a specified number of milliseconds.
+
+setTimeout(() => {
+    console.log("This message is displayed after 2 seconds");
+}, 2000);
+
+// SetInterval is used to execute a function repeatedly until cleared.
+
+setInterval(() => {
+    console.log("This message is displayed every 3 seconds");  
+}, 3000);
